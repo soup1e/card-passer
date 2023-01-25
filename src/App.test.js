81079b48c66da84f361c5ae49f4caab736de83ab
Sam Adams <sam.adams7785@gmail.com>
2023-01-25 13:28:50 -0800

    test passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { UserProvider } from './context/GameContext';
 
 test('renders different players', () => {
-  render(<App />);
+  render(
+    <UserProvider>
+      <App />
+    </UserProvider>
+  );
+
   const player1 = screen.getByText(/Player 1/i);
   const player2 = screen.getByText(/Player 2/i);
   const player3 = screen.getByText(/Player 3/i);
