253351b053c2a01bff93fcceccb38cb58967e4f2
Sam Adams <sam.adams7785@gmail.com>
2023-01-25 12:44:28 -0800

    game context js

---
+++ src/context/GameContext.js
@@ -0,0 +1,16 @@
+import { react, useState, createContext } from 'react';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [selectedCard, setSelectedCard] = useState();
+  const [to, setTo] = useState(1);
+  const [from, setFrom] = useState('deck');
+  return (
+    <UserContext.Provider value={{ to, setTo, from, setFrom, selectedCard, setSelectedCard }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserProvider, UserContext };
